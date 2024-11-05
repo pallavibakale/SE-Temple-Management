7167067c0f2cc968f2b28af60a279de7b9e33b87
Khushal <khushalgarg85886@gmail.com>
2024-11-05 12:48:52 -0500

    appointconfirmation page modified

---
+++ client/src/components/AppointmentConfirmation.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import Navigation from './Navigation'
+import Footer from './Footer'
+import { useNavigate } from 'react-router-dom'
+import { Button } from 'react-bootstrap'
+import "./styles/AppointmentConfirmation.css"
+const Confirmation =()=>{
+
+    return(
+        <div className='confirmation'>
+            <div className='confirmation-section'>
+                <h3> Your Booking Has Been Confirmed</h3>
+                <p>Confirmation receipt has been sent to your email</p>
+                <Button>Back to Homepage</Button>
+            </div>
+
+            <div className='support-section'>
+                <h1 style={{color:'#FF7400 '}}> Support Our Temple</h1>
+                <p style={{width:'700px'}}>Join us in preserving our sacred space and supporting our community by making a generous donation today. Every contribution makes a difference.</p>
+                <Button >Donate Now</Button>
+            </div>
+        </div>
+    )
+}
+
+const AppointmentConfirmation = () => {
+    const navigate = useNavigate();
+    const handleLogout = async () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("role");
+    localStorage.removeItem("empId");
+    localStorage.setItem("role", "");
+    navigate("/");
+  };
+  return (
+    <div className="container text-center">
+      <Navigation onLogout={handleLogout} />
+      <Confirmation />
+      <Footer />
+    </div>
+  )
+}
+
+export default AppointmentConfirmation
